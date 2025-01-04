e3632b39f46d86cfc508095acac58579bcb262eb
Mehmet <mathdenizi@gmail.com>
2025-01-04 06:25:15 +0100

    preparing the empty frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar.tsx";
 import "./App.css";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks.tsx";
+import { Carousel } from "./layouts/HomePage/Carousel.tsx";
+import { Heros } from "./layouts/HomePage/Heros.tsx";
+import { LibraryServices } from "./layouts/HomePage/LibraryServices.tsx";
+import { Footer } from "./layouts/NavbarAndFooter/Footer.tsx";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the fullstack course</h1>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
+      <LibraryServices />
+      <Footer />
     </div>
   );
 }
