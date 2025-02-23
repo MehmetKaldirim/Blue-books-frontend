792d02608e5717f09323d9210876e90635c65d38
Mehmet <mathdenizi@gmail.com>
2025-02-23 02:40:59 +0100

    ready to deploy

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,21 +1,9 @@
-//import { OktaAuth, StorageType, OAuthResponseMode } from "@okta/okta-auth-js";
-
 export const oktaConfig = {
   clientId: process.env.REACT_APP_CLIENT_ID,
   issuer: process.env.REACT_APP_ISSUER,
-  redirectUri: "https://localhost:3000/login/callback",
-  //   responseMode: "query" as OAuthResponseMode, // 🔹 Çerez kullanımını azaltır
-  //   storageManager: {
-  //     token: {
-  //       storageTypes: ["sessionStorage"] as StorageType[], // 🔹 Sadece sessionStorage kullan
-  //     },
-  //     cache: {
-  //       storageTypes: ["localStorage"] as StorageType[], // 🔹 Cache için localStorage kullan
-  //     },
-  //   },
+  redirectUri:
+    "https://blue-book-frontend-36443fa73b00.herokuapp.com/login/callback", // yeni Heroku URL
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true,
 };
-
-//const oktaAuth = new OktaAuth(oktaConfig);
