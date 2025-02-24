f29865f33d623b43a3077f2fc5ab5cbdcd6e7993
Mehmet <mathdenizi@gmail.com>
2025-02-24 02:24:47 +0100

    try to solve okta problem

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,9 +1,9 @@
-export const oktaConfig = {
+const oktaConfig = {
   clientId: process.env.REACT_APP_CLIENT_ID,
   issuer: process.env.REACT_APP_ISSUER,
   redirectUri:
     "https://blue-book-frontend-36443fa73b00.herokuapp.com/login/callback", // yeni Heroku URL
   scopes: ["openid", "profile", "email"],
   pkce: true,
-  disableHttpsCheck: true,
+  disableHttpsCheck: false, // production ortamında false olmalı
 };
